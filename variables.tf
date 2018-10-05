a8599ed3b7e6269338aad64813a8b764bc7fec44
Frederic Jacquet <fred@ljf.ch>
2018-10-05 12:44:38 +0200

    install of LAPS

--- variables.tf
+++ variables.tf
@@ -110,7 +110,7 @@ variable "aws_number" {
     dc         = 1
     dhcp       = 0
     exchange   = 0
-    fs         = 0
+    fs         = 2
     guacamole  = 0
     ipam       = 0
     jumpbox    = 1
