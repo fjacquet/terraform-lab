ee87a491f4242802efd0889eec4f578488af0d18
Frederic Jacquet <fred@ljf.ch>
2018-10-10 21:35:28 +0200

    fix for s3 & adcs

--- post_setup/install-pki-rootca.ps1
+++ post_setup/install-pki-rootca.ps1
@@ -5,7 +5,7 @@ $domain = "evlab.ch"
 $s3bucket = "installers-fja"
 $caname = "evlab Root Certificate Authority"
 $root = "EVLAB-ROOT"
-$url = 'https://raw.githubusercontent.com/fjacquet/terraform-lab/master/config_files/$($file)'
+$url = "https://raw.githubusercontent.com/fjacquet/terraform-lab/master/config_files/$($file)"
 
 Install-Module -Name PSPKI
 # Get the right polica file
@@ -13,39 +13,39 @@ Invoke-WebRequest -Uri $url -OutFile $file #DevSkim: ignore DS104456
 Copy-Item -Path $file -Destination "C:\Windows\capolicy.inf"
 # startup configuration 
 Install-AdcsCertificationAuthority `
-   -CAType StandaloneRootCA `
-   -CACommonName $caname `
-   -KeyLength 4096 `
-   -HashAlgorithm SHA256 `
-   -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" `
-   -ValidityPeriod Years`
--ValidityPeriodUnits 20 `
-   -Force
+    -CAType StandaloneRootCA `
+    -CACommonName $caname `
+    -KeyLength 4096 `
+    -HashAlgorithm SHA256 `
+    -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" `
+    -ValidityPeriod Years `
+    -ValidityPeriodUnits 20 `
+    -Force
 
 # remove default
 $crllist = Get-CACrlDistributionPoint
 foreach ($crl in $crllist) {
-  Remove-CACrlDistributionPoint $crl.uri -Force
+    Remove-CACrlDistributionPoint $crl.uri -Force
 }
 # create valide crl
 Add-CACRLDistributionPoint `
-   -Uri C:\Windows\System32\CertSrv\CertEnroll\$($root)%8%9.crl `
-   -PublishToServer `
-   -PublishDeltaToServer `
-   -Force
+    -Uri C:\Windows\System32\CertSrv\CertEnroll\$($root)%8%9.crl `
+    -PublishToServer `
+    -PublishDeltaToServer `
+    -Force
 Add-CACRLDistributionPoint `
-   -Uri http://pki.$($domain)/pki/$($root)%8%9.crl `
-   -AddToCertificateCDP `
-   -AddToFreshestCrl `
-   -Force
+    -Uri http://pki.$($domain)/pki/$($root)%8%9.crl `
+    -AddToCertificateCDP `
+    -AddToFreshestCrl `
+    -Force
 # Prepare AIA 
 Get-CAAuthorityInformationAccess `
-   | Where-Object { $_.uri -like '*ldap*' -or $_.uri -like '*http*' -or $_.uri -like '*file*' } `
-   | Remove-CAAuthorityInformationAccess `
-   -Force
+    | Where-Object { $_.uri -like '*ldap*' -or $_.uri -like '*http*' -or $_.uri -like '*file*' } `
+    | Remove-CAAuthorityInformationAccess `
+    -Force
 Add-CAAuthorityInformationAccess `
-   -AddToCertificateAia http://pki.$($domain)/pki/$($root)%3%4.crt `
-   -Force
+    -AddToCertificateAia http://pki.$($domain)/pki/$($root)%3%4.crt `
+    -Force
 # Configure 
 certutil.exe –setreg CA\CRLPeriodUnits 20
 certutil.exe –setreg CA\CRLPeriod “Years”
@@ -60,9 +60,16 @@ Restart-Service -Name certsvc
 certutil -crl
 
 # Copy to S3
-Copy-S3Object -LocalFile "C:\Windows\System32\CertSrv\CertEnroll\RootCA_$($caname).crt" `
-   -BucketName $s3bucket `
-   -Key root-ca.crt
-Copy-S3Object -LocalFile "C:\Windows\System32\CertSrv\CertEnroll\$($root).crl" `
-   -BucketName $s3bucket `
-   -Key root-ca.crl
+$certdir = "C:\Windows\System32\CertSrv\CertEnroll"
+$localdir = "c:\installers"
+
+copy-item "$($certdir)\$($env:computername)_$($caname).crt" $localdir
+copy-item "$($certdir)\$($root).crl" $localdir
+
+Write-S3Object -File  "$($localdir)\$($env:computername)_$($caname).crt" `
+    -BucketName $s3bucket `
+    -key root-ca.crt
+
+Write-S3Object -File "$($localdir)\$($root).crl" `
+    -BucketName $s3bucket `
+    -key root-ca.crl
