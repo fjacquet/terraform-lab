7a8718e2bbdee4d13fe6ad72c340085fcce92827
Frederic Jacquet <fred@ljf.ch>
2018-10-12 00:18:07 +0200

    get autospinstall

--- post_setup/get-sharepoint.ps1
+++ post_setup/get-sharepoint.ps1
@@ -1,3 +1,6 @@
 $bin = "SW_DVD5_SharePoint_Server_2016_64Bit_English_MLF_X20-97223.ISO"
 Copy-S3Object -BucketName installers-fja -Key $bin -LocalFile C:\installers\$bin
 Mount-DiskImage -ImagePath C:\installers\$bin
+Set-Location C:\installers\
+git clone https://github.com/brianlala/AutoSPInstaller.git
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/fjacquet/terraform-lab/master/post_setup/config-sp.xml -o config-sp.xml #DevSkim: ignore DS104456 
--- post_setup/install-chocolateys.ps1
+++ post_setup/install-chocolateys.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force  #DevSkim: ignore DS113853
 Install-PackageProvider -Name NuGet  -Force -Confirm:$false
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))#DevSkim: ignore DS104456 
-$values = ('notepadplusplus','googlechrome','sysinternals','7zip.install','baretail','windirstat','curl')
+$values = ('notepadplusplus','googlechrome','sysinternals','7zip.install','baretail','windirstat','curl','git')
 foreach ($value in $values) {
   choco install $value -y
 }
