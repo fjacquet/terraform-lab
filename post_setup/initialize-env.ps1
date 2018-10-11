aec54a7c1017f6feb7578415cc06027cc3eeb585
Frederic Jacquet <fred@ljf.ch>
2018-10-11 23:38:44 +0200

    smbv1 & stop explorer

--- post_setup/initialize-env.ps1
+++ post_setup/initialize-env.ps1
@@ -3,7 +3,7 @@ Set-WinSystemLocale fr-CH
 $langList = New-WinUserLanguageList fr-CH
 Set-WinUserLanguageList $langList -Force
 # Allow WinRM
-Configure-SMremoting.exe -enable
+# Configure-SMremoting.exe -enable
 # Remove SMBv1
 Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol -NoRestart
-Install-Module -Name AWSPowerShell 
+# Install-Module -Name AWSPowerShell 
