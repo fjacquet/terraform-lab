291646f65425224be7bd375662ab39c99edc4ba9
Frederic Jacquet <fred@ljf.ch>
2018-10-13 18:55:46 +0200

    reboot at the end

--- user_data/config-glpi.sh
+++ user_data/config-glpi.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 sudo yum install  wget -y
-# yum update rh-amazon-rhui-client
 
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
 yum-config-manager –-enable --save remi-php73 remi-glpi93 remi
-
-# subscription-manager repos --enable=rhel-7-server-optional-rpms
-
 
 cat > /etc/yum.repos.d/mariadb.repo << EOF
 # MariaDB 10.3 RedHat repository list - created 2018-10-13 16:11 UTC
@@ -20,21 +16,13 @@ gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
 
-yum install MariaDB-server MariaDB-client \
-httpd python jq python2-pip \
-glpi-* php php-gd php-mysql php-mcrypt php-apcu php-xmlrpc \
-php-pecl-zendopcache php-ldap php-imap \
-php-mbstring php-simplexml php-xml -y
-
-
+yum install MariaDB-server MariaDB-client  httpd python jq python2-pip glpi-* php php-gd php-mysql php-mcrypt php-apcu php-xmlrpc php-pecl-zendopcache php-ldap php-imap php-mbstring php-simplexml php-xml -y
+pip install --upgrade pip
+pip install awscli --upgrade --user
+export PATH=$PATH:/root/.local/bin
 HOSTNAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region=$REGION --output=text |awk '{print $5}')
 FQDN="$HOSTNAME.evlab.ch"
 hostnamectl set-hostname $FQDN
-
-
-pip install --upgrade pip
-export PATH=$PATH:/root/.local/bin
-pip install awscli --upgrade --user
 
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) #DevSkim: ignore DS137138
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}') #DevSkim: ignore DS137138
@@ -50,9 +38,5 @@ setenforce 0
 systemctl start httpd mariadb
 systemctl enable httpd mariadb
 systemctl is-enabled httpd mariadb
-# firewall-cmd --zone=public --add-port=http/tcp --permanent
-# firewall-cmd --zone=public --add-port=https/tcp --permanent
-# firewall-cmd --reload
 
-# yum install cpan -y
-# cpan install Fusioninventory::Agent
+reboot
