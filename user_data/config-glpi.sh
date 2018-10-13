311757554d7883256acc841983f8e80f391a44e8
Frederic Jacquet <fred@ljf.ch>
2018-10-13 19:25:09 +0200

    repo manager fails

--- user_data/config-glpi.sh
+++ user_data/config-glpi.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
-sudo yum install  wget -y
+yum upgrade -y 
+yum install wget httpd -y
 
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh epel-release-latest-7.noarch.rpm
+
+yum-config-manager –-enable --save epel
+yum install python2-pip.noarch python34-pip.noarch -y
+yum-config-manager –-disable --save epel
+yum clean all 
+
+pip install --upgrade pip
+pip install awscli --upgrade --user
+export PATH=$PATH:/root/.local/bin
+HOSTNAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region=$REGION --output=text |awk '{print $5}')
+FQDN="$HOSTNAME.evlab.ch"
+hostnamectl set-hostname $FQDN
+
 wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm
-yum-config-manager –-enable --save remi-php73 remi-glpi93 remi
+rpm -Uvh remi-release-7.rpm 
+
+yum-config-manager –-enable --save remi-php73 remi-glpi93 remi 
+yum install  jq glpi-* php php-gd php-mysql php-mcrypt php-apcu php-xmlrpc php-pecl-zendopcache php-ldap php-imap php-mbstring php-simplexml php-xml -y
+yum-config-manager –-disable --save remi-php73 remi-glpi93 remi 
+yum clean all
 
 cat > /etc/yum.repos.d/mariadb.repo << EOF
 # MariaDB 10.3 RedHat repository list - created 2018-10-13 16:11 UTC
@@ -14,15 +33,11 @@ name = MariaDB
 baseurl = http://yum.mariadb.org/10.3/rhel7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
+enabled=1
 EOF
+yum install MariaDB-server MariaDB-client -y
 
-yum install MariaDB-server MariaDB-client  httpd python jq python2-pip glpi-* php php-gd php-mysql php-mcrypt php-apcu php-xmlrpc php-pecl-zendopcache php-ldap php-imap php-mbstring php-simplexml php-xml -y
-pip install --upgrade pip
-pip install awscli --upgrade --user
-export PATH=$PATH:/root/.local/bin
-HOSTNAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --region=$REGION --output=text |awk '{print $5}')
-FQDN="$HOSTNAME.evlab.ch"
-hostnamectl set-hostname $FQDN
+
 
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id) #DevSkim: ignore DS137138
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}') #DevSkim: ignore DS137138
