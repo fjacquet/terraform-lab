df71c269c851ba68dfe7d56ffba9f3ad1e4941b9
Fred Jacquet <fred@ljf.ch>
2021-05-09 21:26:37 +0200

    missing new files

---
+++ backend.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt        = true
+    bucket         = "tf-config"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    region         = "eu-west-1"
+    key            = "evlab/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  access_key = var.access_key
+  region     = var.aws_region
+  secret_key = var.secret_key
+}
+
+data "aws_s3_bucket" "tf-config" {
+  bucket = "tf-config"
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
---
+++ post_setup/create-s2d.ps1
@@ -0,0 +1,22 @@
+$nodes=("EC2AMAZ-2SEE361","EC2AMAZ-6CTPLJM","EC2AMAZ-A5B3OR2","EC2AMAZ-BUI8JUF","EC2AMAZ-F40N9E1","EC2AMAZ-LSUBUHA","EC2AMAZ-OV4RR2R")
+
+Restart-Computer -ComputerName $nodes -Wait -For Wmi -Force
+Install-WindowsFeature -Name RSAT-Clustering
+
+$report = Test-Cluster -Node $nodes -Include 'Storage Spaces Direct', 'Inventory', 'Network', 'System Configuration'
+
+$reportFilePath = $report.FullName
+Start-Process $reportFilePath
+
+
+$vips = ("10.0.0.236", "10.0.0.117", "10.0.0.242", "10.0.0.27", "10.0.0.177", "10.0.0.226", "10.0.0.199")
+New-Cluster -Name S2D -Node $nodes -NoStorage
+
+New-Item -ItemType Directory -Path c:\Shaare\Witness
+[string]$DomainName = (Get-WmiObject win32_computersystem).domain
+New-SmbShare -Name fsw -Path c:\Share\Witness -FullAccess ($DomainName + "\Domain Computers")
+$env:COMPUTERNAME
+Set-ClusterQuorum -Cluster S2D -FileShareWitness \\EC2AMAZ-2UNRVBQ\fsw
+
+
+Enable-ClusterS2D -PoolFriendlyName S2DPool -Confirm:$false -SkipEligibilityChecks:$true -CimSession $nodes[0]
