604529e1682c690000b8c69dfc2b9f875431cc1d
Fred Jacquet <fred@ljf.ch>
2021-09-05 09:42:57 +0200

    prepare for ansible only

--- user_data/config-win.ps1
+++ user_data/config-win.ps1
@@ -1,26 +1,27 @@
 <powershell>
-mkdir C:\installers\
 
-# Install windows features
-add-windowsfeature -Name RSAT
+Initialize-AWSDefaults
+$instanceId = Invoke-RestMethod -Uri http://169.254.169.254/latest/meta-data/instance-id #DevSkim: ignore DS104456
+$instance = (Get-EC2Instance -InstanceId $instanceId).Instances[0]
+$instanceName = ($instance.Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -Expand Value)
 
-$gitroot = 'https://raw.githubusercontent.com/fjacquet/terraform-lab/master/post_setup/'
-Set-ExecutionPolicy Bypass -Scope Process -Force #DevSkim: ignore DS113853
-$scripts = (
-  'Disable-av',
-  'Disable-ieesc',
-  'Initialize-env',
-  'Initialize-winrm',
-  'Install-nbugrt',
-  'Install-chocolateys',
-  'Install-mslaps',
-  'Install-features',
-  # 'Install-fusioninventory',
-  # 'Join-domain-member',
-  'Initialize-hostname'
-)
-foreach ($script in $scripts) {
-  $url = "$($gitroot)$($script).ps1"
-  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($url)) #DevSkim: ignore DS104456
-}
+Rename-Computer -NewName $instanceName -Force -Confirm:$false
+
+$url = 'https://raw.githubusercontent.com/fjacquet/terraform-lab/master/post_setup/ConfigureRemotingForAnsible.ps1'
+Invoke-Expression ((New-Object System.Net.Webclient).DownloadString($url)) #DevSkim: ignore DS104456
+
+
+# winrm quickconfig -q
+# winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+# winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+# winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+# winrm set winrm/config/service/auth '@{Basic="true"}'
+
+# netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+# netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+
+# Stop-Service -Name "WinRM" -ErrorAction Stop
+# Set-Service -Name "WinRM" -StartupType Automatic
+# Start-Service -Name "WinRM" -ErrorAction Stop
+
 </powershell>
